6be9ca2cfae90fd127bbfdc8c8950f100e1b78e2
BlackJeff <x>
2021-02-24 23:23:50 +0200

    Создание проекта

---
+++ ProjectN/Assets/Scripts/MobBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    private float hp;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+    protected virtual void GetDagame(float dmg)
+    {
+        hp -= dmg;
+        if (hp <= 0)
+            Destroy(gameObject);
+    }
+}
---
+++ ProjectN/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MobBehaviour
+{
+
+    private bool isMoving = true;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        if(isMoving)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.position = new Vector3(transform.position.x, 
+                                                 transform.position.y + speed * Time.deltaTime, 0);
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime,
+                                                 transform.position.y, 0);
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,
+                                                 transform.position.y, 0);
+            }
+            if(Input.GetKey(KeyCode.S))
+            {
+                transform.position = new Vector3(transform.position.x,
+                                                 transform.position.y - speed * Time.deltaTime, 0);
+            }
+        }
+    }
+}
